0a710cc4cdaf450c9bcd2540c99cdfabdca57497
jassonjr <jassonjrsdm@gmail.com>
2023-05-26 15:47:04 -0300

    use effect

--- src/components/ListasChaves.jsx
+++ src/components/ListasChaves.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const minhaLista = [
   { id: "1", value: "Fruta" },
   { id: "2", value: "Legume" },
@@ -13,10 +16,27 @@ function ListasChaves() {
   //   <h4 key="5">Item 5</h4>,
   // ];
 
+  const [produtos, setProdutos] = useState(minhaLista);
+  const [pesquisa, setPesquisa] = useState("");
+
+  useEffect(() => {
+    if (pesquisa) {
+      const novaLista = minhaLista.filter((item) => {
+        return item.value.toLowerCase().includes(pesquisa.toLowerCase());
+      });
+      setProdutos(novaLista);
+    } else setProdutos(minhaLista);
+  }, [pesquisa]);
+
   return (
     <div>
       <h1>Minha Lista</h1>
-      {minhaLista.map((item) => {
+      <input
+        value={pesquisa}
+        onChange={(e) => setPesquisa(e.target.value)}
+        placeholder="pesquise aqui"
+      ></input>
+      {produtos.map((item) => {
         return (
           <div key={item.id}>
             <h4>{item.value}</h4>
